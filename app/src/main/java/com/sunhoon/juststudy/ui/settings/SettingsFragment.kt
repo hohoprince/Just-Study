a2294fd1372199e53a92af35d75d3a46dddcc1a8
hohoprince <hoot0512@gmail.com>
2021-05-18 23:35:41 +0900

    설정 화면 구성

--- app/src/main/java/com/sunhoon/juststudy/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/sunhoon/juststudy/ui/settings/SettingsFragment.kt
@@ -22,10 +22,10 @@ class SettingsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+//        val textView: TextView = root.findViewById(R.id.text_notifications)
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
