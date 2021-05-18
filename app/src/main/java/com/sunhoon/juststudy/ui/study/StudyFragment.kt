2d8c84effde58c435c94aef5659c7886001733be
hohoprince <hoot0512@gmail.com>
2021-05-19 00:23:05 +0900

    책상 각도 다이얼로그 구현

--- app/src/main/java/com/sunhoon/juststudy/ui/study/StudyFragment.kt
+++ app/src/main/java/com/sunhoon/juststudy/ui/study/StudyFragment.kt
@@ -1,9 +1,14 @@
 package com.sunhoon.juststudy.ui.study
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -26,6 +31,14 @@ class StudyFragment : Fragment() {
 //        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+
+        val angleLayout = root.findViewById<LinearLayout>(R.id.angle_layout);
+        angleLayout.setOnClickListener {
+            val dlg = Dialog(requireContext())
+            dlg.setContentView(R.layout.dialog_angle)
+            dlg.show()
+        }
+
         return root
     }
 }
